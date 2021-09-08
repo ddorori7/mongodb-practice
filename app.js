f03889b1d9f76f57ba0c1a431971d47bee1cc00f
서지원 <qrd7079@gmail.com>
2021-09-08 13:17:11 +0900

    node 패키지

---
+++ app.js
@@ -0,0 +1,10 @@
+// Express 모듈 불러오기
+const express = require("express");
+const http = require("http"); // 노드 기본 http 모듈
+
+// express 생성
+const app = express();
+
+// 설정, 속성을 집어넣을 때
+// set(키, 값)
+// get(키)
